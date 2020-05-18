de404fd55c3007397bc119fbbe44f0410ba782b8
Amir Haghighati <haghighati.amir@gmail.com>
2020-05-17 23:08:28 -0400

    add heatmap highlight on reload

--- pages/analytics.vue
+++ pages/analytics.vue
@@ -228,6 +228,7 @@ export default {
   mounted() {
     this.resize()
     window.addEventListener('resize', this.resize)
+    this.highlightTopic(this.selectedTopic, true)
     // this.$nextTick(() => {
     //   debugger
     //
@@ -253,7 +254,10 @@ export default {
       this.selectedUser = data.user
     },
     // Just to be consistent with the page compare
-    highlightTopic(item) {
+    highlightTopic(item, explicit = false) {
+      if (explicit)
+        // String is passed instead of an object
+        this.highlightedTopic = item
       this.highlightedTopic = item.id
     },
     updateSelectedTopic(item) {
